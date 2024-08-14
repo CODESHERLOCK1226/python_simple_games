381a53157da78f851850f33bcab2888383ec5b23
ANIKET SAMANTA <169010599+CODESHERLOCK1226@users.noreply.github.com>
2024-08-14 18:59:19 +0530

    Update rock_papers_scissors.py

--- rock_papers_scissors.py
+++ rock_papers_scissors.py
@@ -1,68 +1,48 @@
+rock="""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+
+paper= """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+
+scissors="""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+game_choice=[rock,paper,scissors]
 print("Enter 0 for rock, 1 for paper, 2 for scissors")
 user=int(input())
-if user>2 or user <0:
+if user<=2 and user>=0:
+  print(game_choice[user])
+
+else:
   print("U have entered a invalid character")
-if user==0:
-  print("""
-      _______
-  ---'   ____)
-        (_____)
-        (_____)
-        (____)
-  ---.__(___)
-  """)
-if user==1:
-  print("""
-       _______
-  ---'    ____)____
-             ______)
-            _______)
-           _______)
-  ---.__________)
-  """)
-if user==2:
-  print("""
-      _______
-  ---'   ____)____
-            ______)
-         __________)
-        (____)
-  ---.__(___)
-  """)
+
 if user>=0 and user <=2:
   import random
   computer_choice=random.randint(0,2)
-  if computer_choice==0:
-    print("""
-      _______
-  ---'   ____)
-        (_____)
-        (_____)
-        (____)
-  ---.__(___)
-  """)
-  
-  if computer_choice==1:
-    print("""
-       _______
-  ---'    ____)____
-             ______)
-            _______)
-           _______)
-  ---.__________)
-  """)
-  if computer_choice==2:
-    print("""
-      _______
-  ---'   ____)____
-            ______)
-         __________)
-        (____)
-  ---.__(___)
-  """)
+  print(game_choice[computer_choice])
+
   if user==computer_choice:
     print("Voila! It's a draw")
-  if (user==0 and computer_choice==1) :
+  elif (user==0 and computer_choice==1) :
     print("You lose") 
   if user==1 and computer_choice==2:
     print("You lose")
