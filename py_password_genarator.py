775054ba3604636a4721ba6580e513dd88ee8bb0
ANIKET SAMANTA <169010599+CODESHERLOCK1226@users.noreply.github.com>
2024-08-24 18:04:54 +0530

    Add files via upload

---
+++ py_password_genarator.py
@@ -0,0 +1,20 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+characters1=random.choices(letters, k=nr_letters)
+characters2=random.choices(numbers, k=nr_symbols)
+characters3=random.choices(symbols, k=nr_symbols)
+characters=characters1+characters2+characters3
+random.shuffle(characters)
+password=''
+for item in characters:
+    password+=item+""
+print(password)
+
